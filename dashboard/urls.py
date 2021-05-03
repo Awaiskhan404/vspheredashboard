9160d16348f760565d2ce26ede59835f1a25a99e
Elliot Alderson <elliot@localhost.localdomain>
2021-05-04 03:42:11 +0500

    Elliots turn

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 urlpatterns = [
-    path('dashboard',views.index,name="index"),
+    path('/',views.index,name="index"),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- vsphere/urls.py
+++ vsphere/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('/', include('dashboard.urls')),
+    path('home', include('dashboard.urls')),
 ]
